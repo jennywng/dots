7dd79aa474e6fa244c6956fe5030b15ccc815d37
Jenny <jrw3mx@virginia.edu>
2019-03-21 19:23:32 -0400

    first commit

---
+++ mysql.php
@@ -0,0 +1,92 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dots2";
+
+// create connection to database
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn -> connect_error) {
+	die("Unable to connect to DB: " . $conn -> connect_error);
+}
+
+echo 'connected successfully to dots2 <br>';
+
+
+
+
+$courseODates = "SELECT SC.dN, SC.sID, RCA.oDY FROM system_courses SC 
+INNER JOIN roca_collection_assignments RCA ON SC.id = RCA.cID
+INNER JOIN roca_collections RC ON RC.aID = RCA.id";
+
+// query() function runs the SQL query and puts data into variable result
+$result = $conn->query($courseODates);
+
+echo '<br>';
+
+// if (!$result) {
+// 	echo "Couldn't successfully run query ($courseODates) from DB: " . mysql_error();
+// 	exit;
+// }
+
+// num_rows() checks that theres more than 1 row
+if ($result -> num_rows > 0) {
+	echo "<table><tr><th>SC Name</th><th>sID</th><th>RC oDate</th></tr>";
+	
+	// $cIDArr = array();
+	// $codeArr = array();
+	$row1 = $result->fetch_assoc();
+	print_r($row1);
+	echo '<br>';
+
+
+	while($row = $result->fetch_assoc()) {
+		echo '<tr><td>' . $row['dN'] . '</td><td>' . $row['sID'] . '</td><td>' . $row['oDY'] . '</td></tr>';
+
+		extract($row);
+		$course_name[] = $dN; 
+		$semester[] = $sID;
+		$oDate[] = new Datetime("@$oDY");
+	}
+	echo '</table><br>';
+
+	// print_r($sc_id) . '<br>';
+	// print_r($sc_cid) . '<br>';
+	// print_r($name) . '<br>';
+	// print_r($rc_id) . '<br>';
+	// print_r($date) . '<br>';
+
+	$courses = array_unique($course_name);
+	echo '<br>';
+	// print_r($courses);
+	// echo '<br>';
+
+	// make datetimes pop up depending on which course choice they are for
+	$datetimes[] = null;
+	foreach($oDate as $key=>$d) {
+		echo $d->format('d-m-Y');
+		echo '<br>';
+	}
+
+
+	echo '<select id="course-choice">';
+	echo '<option selected="selected">Choose A Course </option>';
+	foreach($courses as $key=>$course) {
+		echo '<option value="' .$course.'">'.$course.'</option>';
+	}
+	echo "</select>";
+
+
+} else {
+	echo 'No results';
+}
+
+
+$conn->close();
+
+
+// unix timestamps convert date + time
+// ynr, date command
+?>
+
---
+++ sql_statements.php
@@ -0,0 +1,81 @@
+<?php
+
+// get all courses + all the observation dates per course
+// exclude courses that don't have observations
+$courseODates = "SELECT SC.dN, SC.sID, RCA.oDY FROM system_courses SC 
+INNER JOIN roca_collection_assignments RCA ON SC.id = RCA.cID
+INNER JOIN roca_collections RC ON RC.aID = RCA.id";
+
+
+
+// once user choose course + observation date, use that to get the specific data
+// WHERE RC.oDate = date AND SC.cID = course
+
+// select for activities for 1 course and 1 observation date
+$activities = "SELECT CB.ID, CB.cID, CB.code, CB.dName, CB.dDescription, RCD.sT, RCD.eT
+FROM roca_code_bank CB ((((
+INNER JOIN roca_collection_data RCD ON RCD.bID = CB.id)
+INNER JOIN roca_collections RC ON RCD.cID = RC.id)
+INNER JOIN roca_collections_assignments RCA ON RCA.id = RC.aID)
+INNER JOIN systems_courses SC ON SC.id = RCA.cID)
+WHERE RCA.oDY = 1493611200 AND SC.dName = 'MAE 6250' AND RCD.eT != 0"
+
+// select for all activities for 1 course and 1 observation date
+$events = "SELECT CB.ID, CB.cID, CB.code, CB.dName, CB.dDescription, RD.sT
+FROM code_bank CB ((((
+INNER JOIN roca_collection_data RCD ON RCD.bID = CB.id)
+INNER JOIN roca_collections RC ON RCD.cID = RC.id)
+INNER JOIN roca_collection RC ON RD.cID = RC.id)
+INNER JOIN systems_courses SC ON SC.id = RC.cID)
+WHERE RC.oDate = 1498795200 AND SC.cID = '6250-F16' AND RD.eTime == 0"
+
+//select for all intervals for 1 course and 1 observation date
+$intervals = "SELECT CB.ID, CB.cID, CB.code, CB.dName, CB.dDescription, RI.sTime, RI.oID
+FROM code_bank CB ((((
+INNER JOIN roca_intervals RI ON RI.dID = CB.id)
+INNER JOIN roca_data RD ON RD.dID = CB.id)
+INNER JOIN roca_collection RC ON RD.cID = RC.id)
+INNER JOIN systems_courses SC ON SC.id = RC.cID)
+WHERE RC.oDate = 1498795200 AND SC.cID = '6250-F16'"
+
+
+
+/*
+3 JSON files (1 per timeline)
+*/
+
+
+/*
+------------- Examples ----------------
+*/
+
+// number of activities/events that happened in SYS 6001 on Feb 16
+$s1 = "SELECT count(CB.id)
+FROM code_bank CB (((
+INNER JOIN roca_data RD ON RD.dID = CB.id)
+INNER JOIN roca_collection RC ON RD.cID = RC.id)
+INNER JOIN systems_courses SC ON SC.id = RC.cID)
+WHERE SC.cName='SYS 6001' && RC.rDate='Feb 16'
+"
+
+// get lecturing events in SYS 6001
+$s2 = "SELECT CB.dName, CB.dDescription, RD.sTime, RD.eTime
+FROM code_bank CB (((
+INNER JOIN roca_data RD ON RD.dID = CB.id)
+INNER JOIN roca_collection RC ON RD.cID = RC.id)
+INNER JOIN systems_courses SC ON SC.id = RC.cID)
+WHERE SC.cName='SYS 6001' AND CB.id=1
+"
+
+// get interval data
+$s3 = "SELECT CB.dName, CB.dDescription, roca_intervals.sTime, roca_intervals.dID
+FROM roca_intervals RI ((((
+INNER JOIN code_bank CB ON RI.dID = CB.id)
+INNER JOIN roca_data RD ON RD.dID = CB.id)
+INNER JOIN roca_collection RC ON RD.cID = RC.id)
+INNER JOIN systems_courses SC ON SC.id = RC.cID)
+WHERE SC.cName='SYS 6001' && CB.id=1
+"
+
+// $sql = "SELECT ID, cID, code FROM code_bank WHERE cID=7";
+?>
